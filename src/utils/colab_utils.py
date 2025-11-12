49c1bebee8864b561f8516ed058cdb8198242944
chpoonag <63138370+chpoonag@users.noreply.github.com>
2025-11-12 13:29:14 +1000

    Update data path for Kaggle competition input

--- src/utils/colab_utils.py
+++ src/utils/colab_utils.py
@@ -17,8 +17,8 @@ def download_kaggle_competition_data(api_key_dir: str, competition_name: str, ap
     print(f'Data source import complete; path: \n{data_path}')
     
     # Prepare input directory structure for Kaggle competition data
-    new_data_path = f"./kaggle/input/{competition_name}/"
+    new_data_path = f"/content/kaggle/input/{competition_name}/"
     os.makedirs(new_data_path, exist_ok=True)
-    os.system(f"mv {data_path} ./kaggle/input/")
+    os.system(f"mv {data_path} /content/kaggle/input/")
     
     return new_data_path
