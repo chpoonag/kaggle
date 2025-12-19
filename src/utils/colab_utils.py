789ec1b0b0520f4bdf8aa59d5dd3f7d43461c889
chpoonag <63138370+chpoonag@users.noreply.github.com>
2025-12-20 09:01:29 +1300

    Refactor download_kaggle_competition_data function
    Refactor download_kaggle_competition_data function parameters and improve directory handling.

--- src/utils/colab_utils.py
+++ src/utils/colab_utils.py
@@ -1,9 +1,9 @@
-def download_kaggle_competition_data(api_key_dir: str, competition_name: str, api_key_in_google_drive: bool, dst_dir: str = "./kaggle/input/") -> str:
-    import os
-    import kagglehub
-    from google.colab import drive
-    
-    if api_key_in_google_drive:
+import os
+import kagglehub
+from google.colab import drive
+
+def download_kaggle_competition_data(api_key_dir: str, competition_name: str, is_api_key_in_google_drive: bool, dst_dir: str = None) -> str:
+    if is_api_key_in_google_drive:
         # Mount Google Drive to access the API key
         drive.mount('/content/drive')
 
@@ -17,8 +17,9 @@ def download_kaggle_competition_data(api_key_dir: str, competition_name: str, ap
     print(f'Data source import complete; path: \n{data_path}')
     
     # Prepare input directory structure for Kaggle competition data
-    new_data_path = os.path.join(dst_dir, competition_name)
-    os.makedirs(new_data_path, exist_ok=True)
-    os.system(f"mv {data_path} {dst_dir}")
-    
+    if dst_dir is not None:
+        new_data_path = os.path.join(dst_dir, competition_name)
+        os.makedirs(new_data_path, exist_ok=True)
+        os.system(f"mv {data_path} {dst_dir}")
+        print(f'New data path: \n{new_data_path}')
     return new_data_path
